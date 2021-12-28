0cfedb9f0aeca23ebf1ac48ebf8e41018bd1719a
david <wizdave97@gmail.com>
2021-12-28 15:42:01 +0100

    revert previous changes

--- dkg-gadget/src/worker.rs
+++ dkg-gadget/src/worker.rs
@@ -512,6 +512,8 @@ where
 			}
 		}
 
+		try_resume_dkg(self, header);
+
 		try_restart_dkg(self, header);
 
 		self.check_refresh(header);
@@ -1187,10 +1189,6 @@ where
 		loop {
 			let engine = self.gossip_engine.clone();
 			let gossip_engine = future::poll_fn(|cx| engine.lock().poll_unpin(cx));
-
-			if self.latest_header.is_some() && !self.dkg_persistence.initial_check {
-				try_resume_dkg(self, self.latest_header.as_ref().unwrap());
-			}
 
 			futures::select! {
 				notification = self.finality_notifications.next().fuse() => {
