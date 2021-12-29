74c1032db28f8e742362f2f1555386ffe4b60c36
david <wizdave97@gmail.com>
2021-12-29 14:02:46 +0100

    add functions for encryption and decryption

--- dkg-primitives/src/utils.rs
+++ dkg-primitives/src/utils.rs
@@ -3,6 +3,10 @@ use crate::{
 	types::RoundId,
 };
 use bincode::serialize;
+use chacha20poly1305::{
+	aead::{Aead, NewAead},
+	XChaCha20Poly1305,
+};
 use codec::Encode;
 use sc_service::{ChainType, Configuration};
 use serde::{Deserialize, Serialize};
@@ -96,4 +100,71 @@ pub fn store_offline_stage(
 pub fn cleanup(path: PathBuf) -> std::io::Result<()> {
 	fs::remove_file(path)?;
 	Ok(())
+}
+// The secret key bytes should be the byte representation of the secret field of an srr25519 keypair
+pub fn encrypt_data(data: Vec<u8>, secret_key_bytes: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+	if secret_key_bytes.len() != 64 {
+		return Err("Secret key bytes must be 64bytes long")
+	}
+	let key = &secret_key_bytes[..32];
+	// Nonce is 32 bytes, we only need the first 24bytes for the encryption algorithm
+	let nonce = &secret_key_bytes[32..][..24];
+	let cipher = XChaCha20Poly1305::new(key.into());
+
+	let encrypted_data =
+		cipher.encrypt(nonce.into(), &data[..]).map_err(|_| "File encryption failed")?;
+	Ok(encrypted_data)
+}
+
+pub fn decrypt_data(data: Vec<u8>, secret_key_bytes: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+	if secret_key_bytes.len() != 64 {
+		return Err("Secret key bytes must be 64bytes long")
+	}
+
+	let key = &secret_key_bytes[..32];
+	let nonce = &secret_key_bytes[32..][..24];
+	let cipher = XChaCha20Poly1305::new(key.into());
+	let decrypted_data =
+		cipher.decrypt(nonce.into(), &data[..]).map_err(|_| "File decryption failed")?;
+	Ok(decrypted_data)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use sp_keyring::AccountKeyring::Alice;
+
+	fn encrypt(data: Vec<u8>) -> Vec<u8> {
+		let pair = Alice.pair();
+		let key_pair = pair.as_ref();
+		let secret_key = key_pair.secret.to_bytes();
+
+		let encrypted_data = encrypt_data(data, secret_key.to_vec());
+		assert!(encrypted_data.is_ok());
+
+		encrypted_data.unwrap()
+	}
+
+	fn decrypt(data: Vec<u8>) -> Vec<u8> {
+		let pair = Alice.pair();
+		let key_pair = pair.as_ref();
+		let secret_key = key_pair.secret.to_bytes();
+		let decrypted_data = decrypt_data(data, secret_key.to_vec());
+		assert!(decrypted_data.is_ok());
+
+		decrypted_data.unwrap()
+	}
+
+	#[test]
+	fn should_encrypt_and_decrypt_data() {
+		let data = b"Hello world";
+
+		let encrypted_data = encrypt(data.to_vec());
+		let decrypted_data = decrypt(encrypted_data.clone());
+		println!("{:?}", encrypted_data);
+		assert!(encrypted_data != data.to_vec());
+
+		println!("{:?}, {:?}", data, decrypted_data);
+		assert!(decrypted_data == data.to_vec());
+	}
 }
