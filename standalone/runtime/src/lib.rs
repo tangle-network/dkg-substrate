435d86061f9a78b89c696184e08663ddb5127599
Shady Khalifa <shekohex@gmail.com>
2021-12-30 20:00:15 +0200

    use runtime state version zero

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -114,7 +114,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
-	state_version: 1,
+	state_version: 0,
 };
 
 pub mod constants;
