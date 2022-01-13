c21886013a5a366e8a649081aa25994a84007d58
Shady Khalifa <shekohex@gmail.com>
2022-01-13 20:52:41 +0200

    update tests

--- dkg-test-suite/src/testAnchorProposal.ts
+++ dkg-test-suite/src/testAnchorProposal.ts
@@ -11,7 +11,7 @@ import {
 import { ethers } from 'ethers';
 import { keccak256 } from '@ethersproject/keccak256';
 import { ECPair } from 'ecpair';
-import { assert } from '@polkadot/util';
+import { assert, u8aToHex } from '@polkadot/util';
 
 const resourceId = makeResourceId('0xe69a847cd5bc0c9480ada0b339d7f0a8cac2b667', 5002);
 const anchorUpdateProposal: AnchorUpdateProposal = {
@@ -30,7 +30,7 @@ async function testAnchorProposal() {
 
 	await sendAnchorProposal(api);
 
-	await waitNfinalizedBlocks(api, 2, 20 * 5);
+	await waitNfinalizedBlocks(api, 4, 20 * 7);
 
 	const dkgPubKeyCompressed: any = await api.query.dkg.dKGPublicKey();
 	const dkgPubKey = ECPair.fromPublicKey(
@@ -39,12 +39,12 @@ async function testAnchorProposal() {
 	).publicKey.toString('hex');
 
 	const unsubSignedProps: any = await api.query.dKGProposalHandler.signedProposals(
-		5001,
+		5002,
 		{ anchorupdateproposal: 0 },
 		(res: any) => {
 			if (res) {
 				const parsedResult = JSON.parse(JSON.stringify(res));
-				console.log(`Signed anchor prop: ${res.toHuman()}`);
+				console.log(`Signed anchor prop: ${JSON.stringify(parsedResult)}`);
 
 				if (parsedResult) {
 					const sig = parsedResult.anchorUpdateSigned.signature;
@@ -58,9 +58,10 @@ async function testAnchorProposal() {
 					assert(recoveredPubKey == dkgPubKey, 'Public keys should match');
 					if (recoveredPubKey == dkgPubKey) {
 						console.log(`Public keys match`);
+						process.exit(0);
 					} else {
 						console.error(`Public keys do not match`);
-						process.exit();
+						process.exit(-1);
 					}
 				}
 			}
@@ -81,15 +82,12 @@ async function sendAnchorProposal(api: ApiPromise) {
 		api.query.dkg.dKGPublicKey(),
 	]);
 
+	const prop = u8aToHex(encodeUpdateAnchorProposal(anchorUpdateProposal));
 	console.log(`DKG authority set id: ${authoritySetId}`);
 	console.log(`DKG pub key: ${dkgPubKey}`);
 	console.log(`Resource id is: ${resourceId}`);
-	const proposalCall = api.tx.dKGProposals.acknowledgeProposal(
-		0,
-		5001,
-		resourceId,
-		encodeUpdateAnchorProposal(anchorUpdateProposal)
-	);
+	console.log(`Proposal is: ${prop}`);
+	const proposalCall = api.tx.dKGProposals.acknowledgeProposal(0, 5001, resourceId, prop);
 
 	const unsub = await proposalCall.signAndSend(alice, ({ events = [], status }) => {
 		console.log(`Current status is: ${status.type}`);
