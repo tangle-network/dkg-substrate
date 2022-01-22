375ddbca5d9578314bda63f65925faa741de35da
Damilare <dharjeezy@gmail.com>
2022-01-22 01:34:21 +0100

    wrote more benchmarks

--- pallets/dkg-proposals/src/benchmarking.rs
+++ pallets/dkg-proposals/src/benchmarking.rs
@@ -18,9 +18,46 @@ benchmarks! {
 	set_maintainer {
 		let admin: T::AccountId = T::AdminOrigin::successful_origin();
 
-		let maintainer: T::AccountId = account("recipient", 0, SEED);
+		let maintainer: T::AccountId = account("account", 0, SEED);
 	}: _(admin, maintainer)
 	verify {
 		assert_last_event::<T>(Event::MaintainerSet{ old_maintainer: admin, new_maintainer: maintainer }.into());
+	}
+
+	force_set_maintainer {
+		let admin: T::AccountId = T::AdminOrigin::successful_origin();
+
+		let maintainer: T::AccountId = account("account", 0, SEED);
+	}: _(admin, maintainer)
+	verify {
+		assert_last_event::<T>(Event::MaintainerSet{ old_maintainer: admin, new_maintainer: maintainer }.into());
+	}
+
+	set_threshold {
+		let c in 0 .. 16_000;
+
+		let admin: T::AccountId = T::AdminOrigin::successful_origin();
+
+	}: _(admin, c as u32)
+	verify {
+		assert_last_event::<T>(Event::ProposerThresholdChanged { new_threshold: c}.into());
+	}
+
+	add_proposer {
+		let admin: T::AccountId = T::AdminOrigin::successful_origin();
+
+		let v: T::AccountId = account("account", 0, SEED);
+	}: _(admin, v)
+	verify {
+		assert_last_event::<T>(Event::ProposerAdded{ proposer_id: v}.into());
+	}
+
+	remove_proposer {
+		let admin: T::AccountId = T::AdminOrigin::successful_origin();
+
+		let v: T::AccountId = account("account", 0, SEED);
+	}: _(admin, v)
+	verify {
+		assert_last_event::<T>(Event::ProposerRemoved{ proposer_id: v}.into());
 	}
 }
