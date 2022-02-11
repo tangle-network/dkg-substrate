6e56d663cddd90f3fa5512732fe73f5008ea6320
Damilare <dharjeezy@gmail.com>
2022-02-11 20:32:55 +0100

    max ext limit update proposal test mas fee limit update proposal test min withdrawal limit update proposal test

---
+++ dkg-test-suite/src/v-anchor-configurable-limit/testMaxExtLimitUpdate.ts
@@ -0,0 +1,68 @@
+import {ApiPromise} from '@polkadot/api';
+import {Keyring} from '@polkadot/keyring';
+import {
+	encodeVAnchorConfigurableLimitProposal,
+	provider,
+	waitNfinalizedBlocks,
+} from '../util/utils';
+import {keccak256} from '@ethersproject/keccak256';
+import {ECPair} from 'ecpair';
+import {assert, u8aToHex} from '@polkadot/util';
+import {registerResourceId, resourceId, signAndSendUtil, unsubSignedPropsUtil} from "../util/resource";
+import {vAnchorConfigurableLimitProposal} from "../util/proposals";
+
+async function testMaxExtLimitUpdateProposal() {
+	const api = await ApiPromise.create({provider});
+	await registerResourceId(api);
+	await sendMaxExtLimitUpdateProposal(api);
+	console.log('Waiting for the DKG to Sign the proposal');
+	await waitNfinalizedBlocks(api, 8, 20 * 7);
+
+	const dkgPubKeyCompressed: any = await api.query.dkg.dKGPublicKey();
+	const dkgPubKey = ECPair.fromPublicKey(
+		Buffer.from(dkgPubKeyCompressed[1].toHex().substr(2), 'hex'),
+		{compressed: false}
+	).publicKey.toString('hex');
+	const chainIdType = api.createType('DkgRuntimePrimitivesChainIdType', {EVM: 5002});
+	const propHash = keccak256(encodeVAnchorConfigurableLimitProposal(vAnchorConfigurableLimitProposal));
+
+	const proposalType = {maxextlimitupdateproposal: vAnchorConfigurableLimitProposal.header.nonce}
+
+	const unsubSignedProps: any = await unsubSignedPropsUtil(api, chainIdType, dkgPubKey, proposalType, propHash);
+
+	await new Promise((resolve) => setTimeout(resolve, 20000));
+
+	unsubSignedProps();
+}
+
+async function sendMaxExtLimitUpdateProposal(api: ApiPromise) {
+	const keyring = new Keyring({type: 'sr25519'});
+	const alice = keyring.addFromUri('//Alice');
+
+	const [authoritySetId, dkgPubKey] = await Promise.all([
+		api.query.dkg.authoritySetId(),
+		api.query.dkg.dKGPublicKey(),
+	]);
+
+	const prop = u8aToHex(encodeVAnchorConfigurableLimitProposal(vAnchorConfigurableLimitProposal));
+	console.log(`DKG authority set id: ${authoritySetId}`);
+	console.log(`DKG pub key: ${dkgPubKey}`);
+	console.log(`Resource id is: ${resourceId}`);
+	console.log(`Proposal is: ${prop}`);
+	const chainIdType = api.createType('DkgRuntimePrimitivesChainIdType', {EVM: 5001});
+	const kind = api.createType('DkgRuntimePrimitivesProposalProposalKind', 'MaxExtLimitUpdate');
+	const proposal = api.createType('DkgRuntimePrimitivesProposal', {
+		Unsigned: {
+			kind: kind,
+			data: prop
+		}
+	});
+	const proposalCall = api.tx.dKGProposalHandler.forceSubmitUnsignedProposal(proposal);
+
+	await signAndSendUtil(api, proposalCall, alice);
+}
+
+// Run
+testMaxExtLimitUpdateProposal()
+	.catch(console.error)
+	.finally(() => process.exit());
