8ce98113577575755aad79f5ab677d340a4974ac
Damilare <dharjeezy@gmail.com>
2022-03-10 00:17:27 +0100

    doc

--- pallets/dkg-metadata/src/types.rs
+++ pallets/dkg-metadata/src/types.rs
@@ -6,10 +6,10 @@ pub struct RoundMetadata {
 	pub next_round_pub_key: Vec<u8>,
 	pub refresh_signature: Vec<u8>,
 }
-/// different types of aggregated public key
+/// different types of aggregated public keys to submit on chain
 pub enum AggregatedPublicKeyType {
-	/// represents the aggregated public keys on chain at genesis
+	/// represents the aggregated public keys to submit on chain at genesis
 	AggregatedPublicKeysAtGenesis,
-	/// represents the next aggregated public keys on chain
+	/// represents the next aggregated public keys to submit on chain
 	AggregatedPublicKeys,
 }
