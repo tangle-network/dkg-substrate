98f5b99ac4f61c9283be096e55ba01c853d63c8c
dharjeezy <dharjeezy@gmail.com>
2022-03-17 14:30:47 +0100

    E2E tests for add/removing validator and threshold updates (#246)

--- dkg-gadget/src/worker.rs
+++ dkg-gadget/src/worker.rs
@@ -402,15 +402,17 @@ where
 		if next_authorities.id == GENESIS_AUTHORITY_SET_ID {
 			self.dkg_state.listening_for_active_pub_key = true;
 
-			match self.rounds.as_mut().unwrap().start_keygen(latest_block_num) {
-				Ok(()) => {
-					info!(target: "dkg", "Keygen started for genesis authority set successfully");
-					self.active_keygen_in_progress = true;
-				},
-				Err(err) => {
-					error!("Error starting keygen {:?}", &err);
-					self.handle_dkg_error(err);
-				},
+			if let Some(rounds) = self.rounds.as_mut() {
+				match rounds.start_keygen(latest_block_num) {
+					Ok(()) => {
+						info!(target: "dkg", "Keygen started for genesis authority set successfully");
+						self.active_keygen_in_progress = true;
+					},
+					Err(err) => {
+						error!("Error starting keygen {:?}", &err);
+						self.handle_dkg_error(err);
+					},
+				}
 			}
 		}
 	}
