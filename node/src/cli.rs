40cd5f53f183301f4804f1aa8cc0494981440bba
Damilare <dharjeezy@gmail.com>
2022-03-22 13:27:34 +0100

    warnings removed

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -97,11 +97,9 @@ pub struct ExportGenesisWasmCommand {
 }
 
 #[derive(Debug, clap::Parser)]
-#[clap(settings = &[
-	clap::AppSettings::GlobalVersion,
-	clap::AppSettings::ArgsNegateSubcommands,
-	clap::AppSettings::SubcommandsNegateReqs,
-])]
+#[clap(propagate_version = true)]
+#[clap(subcommand_negates_reqs = true)]
+#[clap(args_conflicts_with_subcommands = true)]
 pub struct Cli {
 	#[clap(subcommand)]
 	pub subcommand: Option<Subcommand>,
