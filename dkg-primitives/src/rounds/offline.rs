c507cbe4f8e67bba5305ffe79457879d6f520c2b
Drew Stone <drewstone329@gmail.com>
2022-06-14 12:11:56 -0400

    Fmt

--- dkg-primitives/src/rounds/offline.rs
+++ dkg-primitives/src/rounds/offline.rs
@@ -1,4 +1,3 @@
-use dkg_runtime_primitives::OFFLINE_TIMEOUT;
 // Copyright 2022 Webb Technologies Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,8 @@ use dkg_runtime_primitives::OFFLINE_TIMEOUT;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
+use dkg_runtime_primitives::OFFLINE_TIMEOUT;
 use log::{debug, error, info, trace, warn};
 use round_based::{IsCritical, Msg, StateMachine};
 
