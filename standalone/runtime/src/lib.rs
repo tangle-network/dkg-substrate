7de67979ddf692b346ded02bc485541bdee55428
Shady Khalifa <dev+github@shadykhalifa.me>
2022-06-27 18:41:15 +0200

    Restore lib.rs

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -863,7 +863,7 @@ impl_runtime_apis! {
 
 	fn queued_authority_set() -> dkg_runtime_primitives::AuthoritySet<DKGId> {
 	  let queued_authorities = DKG::next_authorities();
-	  let queued_authority_set_id = DKG::next_authority_set_id();
+	  let queued_authority_set_id = DKG::authority_set_id() + 1u64;
 
 	  dkg_runtime_primitives::AuthoritySet {
 		authorities: queued_authorities,
