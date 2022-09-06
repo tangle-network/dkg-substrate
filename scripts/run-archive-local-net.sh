1563cd396bbc1a221c03ec8a7fe4637d16158bcf
ahmed korim <ahmedkorrim@gmail.com>
2022-09-06 10:13:08 +0200

    Fix Archive node script

--- scripts/run-archive-local-net.sh
+++ scripts/run-archive-local-net.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-RUST_LOG=dkg=trace ./target/release/dkg-standalone-node   --tmp --alice   --pruning=archive --rpc-cors=all &
-RUST_LOG=dkg=trace ./target/release/dkg-standalone-node   --tmp --bob --pruning=archive --rpc-cors=all &
-RUST_LOG=dkg=trace ./target/release/dkg-standalone-node  --tmp --charlie --pruning=archive --rpc-cors=all &
+RUST_LOG=dkg=trace ./target/release/dkg-standalone-node  --base-path=./tmp/alice  --alice   --pruning=archive --rpc-cors=all &
+RUST_LOG=dkg=trace ./target/release/dkg-standalone-node  --base-path=./tmp/bob  --bob --pruning=archive --rpc-cors=all &
+RUST_LOG=dkg=trace ./target/release/dkg-standalone-node  --base-path=./tmp/charlie --charlie --pruning=archive --rpc-cors=all &
