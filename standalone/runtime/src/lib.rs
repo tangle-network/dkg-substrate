7d5a119f670b7e6e7b4e1cef3e39f460a3ecd64c
ahmed korim <ahmedkorrim@gmail.com>
2022-09-21 17:12:42 +0200

    Adding the identity pallet for standalone runtime

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -209,6 +209,30 @@ parameter_types! {
   pub const IndexDeposit: Balance = DOLLARS;
 }
 
+
+parameter_types! {
+	pub const BasicDeposit: Balance = deposit(1, 258);
+	pub const FieldDeposit: Balance = deposit(0, 66);
+	pub const SubAccountDeposit: Balance = deposit(1, 53);
+	pub const MaxSubAccounts: u32 = 100;
+	pub const MaxAdditionalFields: u32 = 100;
+	pub const MaxRegistrars: u32 = 20;
+}
+
+impl pallet_identity::Config for Runtime {
+	type Event = Event;
+	type Currency = Balances;
+	type BasicDeposit = BasicDeposit;
+	type FieldDeposit = FieldDeposit;
+	type SubAccountDeposit = SubAccountDeposit;
+	type MaxSubAccounts = MaxSubAccounts;
+	type MaxAdditionalFields = MaxAdditionalFields;
+	type MaxRegistrars = MaxRegistrars;
+	type Slashed = ();
+	type ForceOrigin = EnsureRoot<Self::AccountId>;
+	type RegistrarOrigin = EnsureRoot<Self::AccountId>;
+	type WeightInfo = ();
+}
 impl pallet_indices::Config for Runtime {
 	type AccountIndex = AccountIndex;
 	type Currency = Balances;
@@ -668,7 +692,6 @@ where
 }
 
 parameter_types! {
-	pub const MaxAdditionalFields: u32 = 5;
 	pub const MaxResources: u32 = 32;
 }
 
@@ -708,6 +731,8 @@ construct_runtime!(
 	Session: pallet_session,
 	Historical: pallet_session_historical,
 	BridgeRegistry: pallet_bridge_registry::<Instance1>,
+	Identity: pallet_identity::{Pallet, Call, Storage, Event<T>}
+
   }
 );
 
