5b2b6dfd3662f4a89a00e382a28dbede94813077
Dustin Brickwood <dustinbrickwood204@gmail.com>
2022-10-02 12:39:46 -0500

    chore: fix keys script

--- keys.sh
+++ keys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "`\n`****************** NODE-1 KEY INSERTION ******************`\n`"
+echo "****************** NODE-1 KEY INSERTION ******************"
 
 ./target/release/dkg-standalone-node key insert --base-path /tmp/blockstorage/standalone-1 \
 --chain "./chainspecs/arana-alpha-raw.json" \
@@ -32,7 +32,12 @@ echo "`\n`****************** NODE-1 KEY INSERTION ******************`\n`"
 --suri "gown surprise mirror hotel cash alarm raccoon you frog rose midnight enter//webb//0//dkg" \
 --key-type wdkg
 
-echo "`\n`****************** NODE-2 KEY INSERTION ******************`\n`"
+echo "node-1 keys inserted into path: /tmp/blockstorage/standalone-1 \n"
+for file in /tmp/blockstorage/standalone-1/chains/arana-alpha-1/keystore/*; do
+  echo "${file##*/}"
+done
+
+echo "\n ****************** NODE-2 KEY INSERTION ******************"
 
 ./target/release/dkg-standalone-node key insert --base-path /tmp/blockstorage/standalone-2 \
 --chain "./chainspecs/arana-alpha-raw.json" \
@@ -64,8 +69,13 @@ echo "`\n`****************** NODE-2 KEY INSERTION ******************`\n`"
 --suri "gown surprise mirror hotel cash alarm raccoon you frog rose midnight enter//webb//1//dkg" \
 --key-type wdkg
 
+echo "node-2 keys inserted into path: /tmp/blockstorage/standalone-2 \n"
+for file in /tmp/blockstorage/standalone-2/chains/arana-alpha-1/keystore/*; do
+  echo "${file##*/}"
+done
 
-echo "`\n`****************** NODE-3 KEY INSERTION ******************`\n`"
+
+echo "\n ****************** NODE-3 KEY INSERTION ******************"
 
 ./target/release/dkg-standalone-node key insert --base-path /tmp/blockstorage/standalone-3 \
 --chain "./chainspecs/arana-alpha-raw.json" \
@@ -96,3 +106,8 @@ echo "`\n`****************** NODE-3 KEY INSERTION ******************`\n`"
 --scheme Ecdsa \
 --suri "gown surprise mirror hotel cash alarm raccoon you frog rose midnight enter//webb//2//dkg" \
 --key-type wdkg
+
+echo "node-3 keys inserted into path: /tmp/blockstorage/standalone-3 \n"
+for file in /tmp/blockstorage/standalone-3/chains/arana-alpha-1/keystore/*; do
+  echo "${file##*/}"
+done
