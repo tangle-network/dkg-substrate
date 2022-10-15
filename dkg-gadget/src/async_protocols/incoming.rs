bb6d04b7ab4499c803cca98965b0429d0c5cef61
Akilesh Tangella <saiakilesh@gmail.com>
2022-10-15 17:30:36 -0400

    updates

--- dkg-gadget/src/async_protocols/incoming.rs
+++ dkg-gadget/src/async_protocols/incoming.rs
@@ -84,7 +84,7 @@ impl TransformIncoming for Arc<SignedDKGMessage<Public>> {
 							.verify_signature_against_authorities(self)
 							.map(|body| Some(Msg { sender, receiver: None, body }))
 					} else {
-						log::warn!(target: "dkg", "Will skip passing message to state machine since not for this round, msg round {:?} this round {:?}", self.msg.round_id, this_round_id);
+						log::warn!(target: "dkg", "Will skip passing message to state machine since not for this round, msg round {:?} this round {:?}", self.msg.session_id, this_session_id);
 						Ok(None)
 					}
 				} else {
