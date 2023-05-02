93b432185e92ac8fd78d5423a656660bb3ac2d79
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-02 19:43:36 +0300

    Add Nix flake development env

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Webb DKG Substrate development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Rust
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        lib = pkgs.lib;
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "dkg";
+          nativeBuildInputs = [
+            pkgs.gmp
+            pkgs.protobuf
+            pkgs.pkg-config
+            # Mold Linker for faster builds (only on Linux)
+            (lib.optionals pkgs.stdenv.isLinux pkgs.mold)
+          ];
+          buildInputs = [
+            # We want the unwrapped version, wrapped comes with nixpkgs' toolchain
+            pkgs.rust-analyzer-unwrapped
+            # Nodejs for test suite
+            pkgs.nodejs_18
+            pkgs.nodePackages.yarn
+            # Finally the toolchain
+            toolchain
+          ];
+          packages = [ ];
+        };
+        # Environment variables
+        RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+      });
+}
