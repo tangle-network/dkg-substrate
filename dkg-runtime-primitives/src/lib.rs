6e4296f24a7c52338d7f2907b50152f9158351c3
drewstone <drewstone329@gmail.com>
2023-06-21 12:39:23 -0400

    Update dkg-runtime-primitives/src/lib.rs

--- dkg-runtime-primitives/src/lib.rs
+++ dkg-runtime-primitives/src/lib.rs
@@ -325,7 +325,7 @@ sp_api::decl_runtime_apis! {
 		fn next_pub_key_sig() -> Option<Vec<u8>>;
 		/// Get next nonce value for refresh proposal
 		fn refresh_nonce() -> u32;
-		/// Returns (true, false) if we should execute an new keygen.
+		/// Returns (true, false) if we should execute a new keygen.
 		/// Returns (true, true) if we should execute a forced new keygen.
 		fn should_execute_new_keygen() -> (bool, bool);
 	}
