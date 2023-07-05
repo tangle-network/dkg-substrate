1fba31495e83b409e27733376c22a91dc03eb0d7
Thomas Braun <braunth@oregonstate.edu>
2023-07-05 13:16:20 -0400

    perform actual yarn commands

--- scripts/ensure_types_unchanged.sh
+++ scripts/ensure_types_unchanged.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 cd $(git rev-parse --show-toplevel)/types
+yarn update:metadata
+yarn build:interfaces
+
 if [[ $(git status --porcelain | grep -v "yarn.lock\|package.json" | grep "M") ]]; then
     echo "The following types have changed"
     git status --porcelain | grep -v "yarn.lock\|package.json" | grep "M"
